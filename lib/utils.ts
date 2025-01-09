0afae7bf1474aa76e9b474f4eb8d831b7a5e0296
Hiren <hiren.godhasara@gmail.com>
2025-01-09 18:20:46 +0530

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,6 +7,7 @@ export function cn(...inputs: ClassValue[]) {
 
 export const GITHUB_TOKEN = process.env.NEXT_PUBLIC_TOKEN;
 export const GITHUB_GIST_ID = "2ff4b4bfe462c984fc0ed92212bdaffd";
+
 export const GIST_FILE_NAME = "data.json"
 
 
