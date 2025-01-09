108076a820164b5e848cc9cebad7fb352c77c341
Hiren <hiren.godhasara@gmail.com>
2025-01-09 17:31:22 +0530

    a

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,7 +6,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const GITHUB_TOKEN = "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";
+export const GITHUB_TOKEN = "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";
 export const GITHUB_GIST_ID = "2ff4b4bfe462c984fc0ed92212bdaffd";
 export const GIST_FILE_NAME = "data.json"
 
